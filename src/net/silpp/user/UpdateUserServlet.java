b301f2a0f7b51c2f139de8847dbe979e2024385b
ijuhom <2014brand@naver.com>
2019-02-25 21:08:15 +0900

    개인정보 수정 보안처리.

--- src/net/silpp/user/UpdateUserServlet.java
+++ src/net/silpp/user/UpdateUserServlet.java
@@ -8,14 +8,29 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-@WebServlet("/update")
+@WebServlet("/update")//(브라우저를 통하지 않고) 직접 호출시  서블릿 관여를 할수있는 문제점이있다. 
 public class UpdateUserServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
-		
+		HttpSession session =req.getSession();
+		//login 유무 담당.
+		Object obj = session.getAttribute(LoginSevlet.SESSION_USER_ID);
+		if(obj==null) {
+			resp.sendRedirect("/slipp/ex1.jsp");
+			return ;
+		}
+		//세션의 id값과 현재id값이 일치하는 여부.
+		String sessionUserId = (String)obj;
 		String userId = req.getParameter("userId");
+		if(!sessionUserId.equals(userId)) {
+			
+			resp.sendRedirect("/slipp/ex1.jsp");
+			return ;
+		}
+		
 		String password= req.getParameter("password");
 		String name= req.getParameter("name");
 		String email= req.getParameter("email");
