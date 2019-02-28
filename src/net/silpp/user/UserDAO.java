a232aa34180c458139b260ceacba83f36129a44a
ijuhom <2014brand@naver.com>
2019-02-28 22:14:12 +0900

    Jdbc템플릿메서드의 이슈 및  interface활용 리팩토링 1-2

--- src/net/silpp/user/UserDAO.java
+++ src/net/silpp/user/UserDAO.java
@@ -6,12 +6,14 @@ import java.sql.SQLException;
 
 import net.slipp.support.JdbcTemplate;
 import net.slipp.support.PreparesetParameters;
+import net.slipp.support.RowMapper;
 
 public class UserDAO {
 
 	/**
-	 * 이슈 - 템플릿 메서드 클래스들의 병합으로 인해 굳이 구현하지않아도 되는 추상메서드 구현. 해결책 1. Jdbc의 추상메서드패턴 클래스 의
-	 * 추상메서드를 각각의 interface로 분리.
+	 * 이슈 - 템플릿 메서드 클래스들의 병합으로 인해 굳이 구현하지않아도 되는 추상메서드 구현.
+	 * 해결책 1. Jdbc의 추상메서드패턴 클래스 의 추상메서드를 각각의 interface로 분리.
+	 * 2. 추상클래스가 사라지면서 객체를 만들어 활용
 	 */
 	public void addUser(User user) throws SQLException {
 		PreparesetParameters pss = new PreparesetParameters() {
@@ -25,89 +27,50 @@ public class UserDAO {
 
 			}
 		};
-		JdbcTemplate template = new JdbcTemplate() {
-			@Override
-			public void setParameters(PreparedStatement pstmt) throws SQLException {
-				pstmt.setString(1, user.getUserId());
-				pstmt.setString(2, user.getPassword());
-				pstmt.setString(3, user.getName());
-				pstmt.setString(4, user.getEmail());
-			}
-
-			@Override
-			public Object mapRow(ResultSet rs) throws SQLException {
-
-				return null;
-			}
-
-		};
+		JdbcTemplate template = new JdbcTemplate();
 
 		String query = "insert into userTbl values (?,?,?,?)";
-		template.exequteUpdate(query,pss);
+		template.exequteUpdate(query, pss);
 	}
 
 	public User findByUserId(String userId) throws SQLException {
-		JdbcTemplate template = new JdbcTemplate() {
-
+		PreparesetParameters pss = new PreparesetParameters() {
 			@Override
 			public void setParameters(PreparedStatement pstmt) throws SQLException {
 				pstmt.setString(1, userId);
-
-			}
-
-			@Override
-			public User mapRow(ResultSet rs) throws SQLException {
-				if (!rs.next()) {
-					return null;
-				}
-				return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-						rs.getString("email"));
-
 			}
 		};
-
-		String query = "select * from usertbl where userId = ? ";
-		return (User) template.executeQuery(query);
-	}
-
-	public void removeUser(String userId) throws SQLException {
-	PreparesetParameters pss = new PreparesetParameters() {
-		
-		@Override
-		public void setParameters(PreparedStatement pstmt) throws SQLException {
-			pstmt.setString(1, userId);
-			
-		}
-	};
-		JdbcTemplate template = new JdbcTemplate() {
-
-			@Override
-			public void setParameters(PreparedStatement pstmt) throws SQLException {
-				pstmt.setString(1, userId);
-			}
+		RowMapper rm = new RowMapper() {
 
 			@Override
 			public Object mapRow(ResultSet rs) throws SQLException {
-				return null;
+				 
+				return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+						rs.getString("email"));
 			}
 		};
+		JdbcTemplate template = new JdbcTemplate();
+
+		String query = "select * from usertbl where userId = ? ";
+		return (User) template.executeQuery(query,pss,rm);
+	}
+
+	public void removeUser(String userId) throws SQLException {
+		PreparesetParameters pss = new PreparesetParameters() {
+
+			@Override
+			public void setParameters(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, userId);
+
+			}
+		};
+		JdbcTemplate template = new JdbcTemplate();
 		String query = "delete from userTbl where userId = ?";
-		template.exequteUpdate(query , pss);
+		template.exequteUpdate(query, pss);
 	}
 
 	public void updateUser(User user) throws SQLException {
 		PreparesetParameters pss = new PreparesetParameters() {
-			
-			@Override
-			public void setParameters(PreparedStatement pstmt) throws SQLException {
-				pstmt.setString(1, user.getPassword());
-				pstmt.setString(2, user.getName());
-				pstmt.setString(3, user.getEmail());
-				pstmt.setString(4, user.getUserId());
-				
-			}
-		};
-		JdbcTemplate template = new JdbcTemplate() {
 
 			@Override
 			public void setParameters(PreparedStatement pstmt) throws SQLException {
@@ -117,13 +80,9 @@ public class UserDAO {
 				pstmt.setString(4, user.getUserId());
 
 			}
-
-			@Override
-			public Object mapRow(ResultSet rs) throws SQLException {
-				return null;
-			}
 		};
+		JdbcTemplate template = new JdbcTemplate();
 		String query = "update userTbl set password =? ,name =? ,email =? where userId = ?";
-		template.exequteUpdate(query , pss );
+		template.exequteUpdate(query, pss);
 	}
 }
--- src/net/slipp/support/JdbcTemplate.java
+++ src/net/slipp/support/JdbcTemplate.java
@@ -10,36 +10,13 @@ import net.silpp.user.User;
 import net.silpp.user.UserDAO;
 
 /**
- * 템플릿 메서드 패턴 클래스
- * JdbcTemplate 과 UpdateJdbcTemplate 의 병합 
- * 단위테스트는 점진적으로 하여 결과를 얻는다.
- * (overloading 을 사용해보자.)
+ * 템플릿 메서드 패턴 클래스 JdbcTemplate 과 UpdateJdbcTemplate 의 병합 단위테스트는 점진적으로 하여 결과를 얻는다.  
+ *  (overloading 을 사용해보자.)
+ *  추상클메서드 부분이  interface로 변환되면서 기존 추상클래스를  > 클래스로 바꿔준다.
  */
-public abstract class JdbcTemplate {
-	
-	public void exequteUpdate(String query) throws SQLException {
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		try {
-			connection = ConnectionManager.getConnection();
-			pstmt = connection.prepareStatement(query);
-			setParameters(pstmt);
-			
-			pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (pstmt != null)
-					pstmt.close();
-				if (connection != null)
-					connection.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
-	}
-	public void exequteUpdate(String query,PreparesetParameters pss) throws SQLException {
+public class JdbcTemplate {
+
+	public void exequteUpdate(String query, PreparesetParameters pss) throws SQLException {
 		Connection connection = null;
 		PreparedStatement pstmt = null;
 		try {
@@ -62,7 +39,7 @@ public abstract class JdbcTemplate {
 		}
 	}
 
-	public Object executeQuery(String query) throws SQLException {
+	public Object executeQuery(String query, PreparesetParameters pss, RowMapper rm) throws SQLException {
 
 		Connection connection = null;
 		PreparedStatement pstmt = null;
@@ -71,13 +48,16 @@ public abstract class JdbcTemplate {
 		try {
 			connection = ConnectionManager.getConnection();
 			pstmt = connection.prepareStatement(query);
-			setParameters(pstmt);
+			pss.setParameters(pstmt);
 			rs = pstmt.executeQuery();
-
-			return mapRow(rs);
+			if (!rs.next()) {
+				return null;
+			}
+			return rm.mapRow(rs);
 
 		} finally {
 			try {
+
 				if (rs != null)
 					rs.close();
 				if (pstmt != null)
@@ -90,7 +70,4 @@ public abstract class JdbcTemplate {
 		}
 	}
 
-	public abstract Object mapRow(ResultSet rs) throws SQLException;
-
-	public abstract void setParameters(PreparedStatement pstmt) throws SQLException;
 }
